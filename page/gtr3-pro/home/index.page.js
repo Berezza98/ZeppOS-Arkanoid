444f52a13a56472bd79d9e779c373b83d631f9c2
Роман Шухевич <roman.berezdetskyi@megogo.net>
2022-06-18 09:03:48 +0300

    clean some parts of code

--- page/gtr3-pro/home/index.page.js
+++ page/gtr3-pro/home/index.page.js
@@ -1,12 +1,7 @@
-import { TEXT_STYLE } from './index.style';
 import Game from '../../../components/Game';
 
-const logger = DeviceRuntimeCore.HmLogger.getLogger('helloworld')
 Page({
   build() {
-    logger.debug('page build invoked')
-    console.log('FIRST RENDER');
-    // const text = hmUI.createWidget(hmUI.widget.TEXT, TEXT_STYLE);
     const game = new Game();
     game.run();
   },
